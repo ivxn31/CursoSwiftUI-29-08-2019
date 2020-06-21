9b58399c1c71ca450028bae6a212cadc1074c257
ivant <ivant@capmega.com>
2020-06-21 18:18:33 -0500

    estructura de carpetas

--- CursoSwiftUI-29-08-2019/ui/DriverScreen.swift
+++ CursoSwiftUI-29-08-2019/ui/DriverScreen.swift
@@ -9,14 +9,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DriverScreen: View {
     var body: some View {
-        Text("Hello, World!")
+        Image(uiImage: .mercedes)
+        .resizable()
+        .frame(height:200)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DriverScreen()
     }
 }
