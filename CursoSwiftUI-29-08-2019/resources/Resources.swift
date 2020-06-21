cc7f47acc77e705538536c0566adde9118d9ca67
ivant <ivant@capmega.com>
2020-06-21 17:43:23 -0500

    definicion de recursos para el proyecto

---
+++ CursoSwiftUI-29-08-2019/resources/Resources.swift
@@ -0,0 +1,103 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 21/06/20
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import UIKit
+
+extension UIImage{
+    
+    static var bottas:UIImage{
+        get{
+            return UIImage(named: "bottas")!
+        }
+    }
+    
+    static var ferrari:UIImage{
+        get{
+            return UIImage(named: "ferrari")!
+        }
+    }
+    
+    static var mclaren:UIImage{
+        get{
+            return UIImage(named: "mclaren")!
+        }
+    }
+    
+    static var mercedes:UIImage{
+        get{
+            return UIImage(named: "mercedes")!
+        }
+    }
+    
+    static var redbull:UIImage{
+        get{
+            return UIImage(named: "redbull")!
+        }
+    }
+    
+    static var renault:UIImage{
+        get{
+            return UIImage(named: "renault")!
+        }
+    }
+    
+    static var gasly:UIImage{
+        get{
+            return UIImage(named: "gasly")!
+        }
+    }
+    
+    static var hamilton:UIImage{
+        get{
+            return UIImage(named: "hamilton")!
+        }
+    }
+    
+    static var hulkenberg:UIImage{
+        get{
+            return UIImage(named: "hulkenberg")!
+        }
+    }
+    
+    static var leclerc:UIImage{
+        get{
+            return UIImage(named: "leclerc")!
+        }
+    }
+    
+    static var norris:UIImage{
+        get{
+            return UIImage(named: "norris")!
+        }
+    }
+    
+    static var ricciardo:UIImage{
+        get{
+            return UIImage(named: "ricciardo")!
+        }
+    }
+    
+    static var sainz:UIImage{
+        get{
+            return UIImage(named: "sainz")!
+        }
+    }
+    
+    static var verstappen:UIImage{
+        get{
+            return UIImage(named: "verstappen")!
+        }
+    }
+    
+    static var vettel:UIImage{
+        get{
+            return UIImage(named: "vettel")!
+        }
+    }
+}
