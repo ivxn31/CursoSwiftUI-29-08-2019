7996af4707820e69ba0fc8572eb8bd14d801e038
ivant <ivant@capmega.com>
2020-06-21 17:05:15 -0500

    Initial Commit

---
+++ CursoSwiftUI-29-08-2019/ContentView.swift
@@ -0,0 +1,22 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 21/06/20
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
