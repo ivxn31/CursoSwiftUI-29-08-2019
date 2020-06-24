76f7cf8c47361a71df8a93da58b35423037b7fed
ivant <ivant@capmega.com>
2020-06-24 11:11:22 -0500

    Navegar entre pantallas y visualizar el diseño en diferentes dispositovos el DriverScreen

---
+++ CursoSwiftUI-29-08-2019/ui/DriversScreen.swift
@@ -0,0 +1,22 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 24/06/20
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import SwiftUI
+
+struct DriversScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DriversScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DriversScreen()
+    }
+}
